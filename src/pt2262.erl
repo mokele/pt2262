eecef2853b7963d0c402346fed0d9af5b9a2aa05
Steven Gravell <steve@mokele.co.uk>
2013-05-07 13:43:22 +0100

    remove pie as a dependency

--- src/pt2262.erl
+++ src/pt2262.erl
@@ -4,18 +4,18 @@
     encode/2
   ]).
 
--include_lib("pie/include/pie.hrl").
-
 -define(SHORT, 133).
 -define(LONG, 400).
+-define(LOW, 0).
+-define(HIGH, 1).
 
 encode(Sig, Cmd) -> 
   Msg0 = lists:foldl(fun(_, Acc) -> 
       L = lists:foldl(fun(I, Acc0) -> 
           if ((Sig bsr (15-I)) band 1) =/= 0 -> 
-              [?SHORT, ?PIE_LOW, ?LONG, ?PIE_HIGH | Acc0]; 
+              [?SHORT, ?LOW, ?LONG, ?HIGH | Acc0]; 
             true -> 
-              [?LONG, ?PIE_LOW, ?SHORT, ?PIE_HIGH | Acc0] 
+              [?LONG, ?LOW, ?SHORT, ?HIGH | Acc0] 
           end 
         end, 
         Acc,
@@ -23,15 +23,15 @@ encode(Sig, Cmd) ->
       ),
       R = lists:foldl(fun(I, Acc0) -> 
           if ((Cmd bsr (7-I)) band 1) =/= 0 ->
-              [?SHORT, ?PIE_LOW, ?LONG, ?PIE_HIGH | Acc0]; 
+              [?SHORT, ?LOW, ?LONG, ?HIGH | Acc0]; 
             true ->
-              [?LONG, ?PIE_LOW, ?SHORT, ?PIE_HIGH | Acc0] 
+              [?LONG, ?LOW, ?SHORT, ?HIGH | Acc0] 
           end
         end,
         L,
         lists:seq(0, 7)
       ),
-      [?LONG + 5000, ?PIE_LOW, ?SHORT, ?PIE_HIGH | R]
+      [?LONG + 5000, ?LOW, ?SHORT, ?HIGH | R]
     end,
     [],
     lists:seq(0, 15)
